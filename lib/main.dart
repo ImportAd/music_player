cfe1034037472d55cbeed801746ff542908f5401
ImportAd <musin.f.mail@gmail.com>
2023-09-18 14:32:05 +0300

    feature
    adding Material3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
-import 'UI/home_screen/home.dart';
+import 'package:get/get.dart';
+import 'package:peace_img/screens/home/home.dart';
 
 void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
