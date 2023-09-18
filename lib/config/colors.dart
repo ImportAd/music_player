e7461c83f2d97d8a9d8df8660b00ca7926bb8a45
ImportAd <musin.f.mail@gmail.com>
2023-09-18 14:27:48 +0300

    feature
    placing application colors in a separate file

---
+++ lib/config/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const backgroundBlack = Color.fromRGBO(31, 41, 51, 1);
+  static const backgroundallBlack = Color(0xff070B11);
+  static const white = Color.fromRGBO(245, 247, 250, 1);
+  static const black = Color(0xff1F112C);
+}
