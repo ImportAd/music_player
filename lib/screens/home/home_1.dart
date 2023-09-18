9f533aa40092ec3431d0ae3ff008dbcd4f2eba1f
ImportAd <musin.f.mail@gmail.com>
2023-09-18 14:47:29 +0300

    chore
    unnecessary file

---
+++ lib/screens/home/home_1.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+
+// class SearchBarApp extends StatefulWidget {
+//   const SearchBarApp({super.key});
+
+//   @override
+//   State<SearchBarApp> createState() => _SearchBarAppState();
+// }
+
+// class _SearchBarAppState extends State<SearchBarApp> {
+//   bool isDark = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final ThemeData themeData = ThemeData(
+//         useMaterial3: true,
+//         brightness: isDark ? Brightness.dark : Brightness.light);
+
+//     return Scaffold(
+//       appBar: ,
+//     );
+//   }
+// }
